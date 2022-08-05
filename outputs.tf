df0d87ae21a280b486cbb00ebf7a8fc52a083891
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2022-08-05 14:51:16 -0700

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,37 @@
 
 
-output "efs_arn" {
-  value       = module.efs.arn
-  description = "EFS ARN"
-}
+# output "efs_arn" {
+#   value       = module.efs.arn
+#   description = "EFS ARN"
+# }
 
-output "efs_id" {
-  value       = module.efs.id
-  description = "EFS ID"
-}
+# output "efs_id" {
+#   value       = module.efs.id
+#   description = "EFS ID"
+# }
 
-output "efs_host" {
-  value       = module.efs.host
-  description = "Route53 DNS hostname for the EFS"
-}
-
-
+# output "efs_host" {
+#   value       = module.efs.host
+#   description = "Route53 DNS hostname for the EFS"
+# }
 
 
-output "efs_mount_target_ids" {
-  value       = module.efs.mount_target_ids
-  description = "List of EFS mount target IDs (one per Availability Zone)"
-}
 
-output "efs_mount_target_ips" {
-  value       = module.efs.mount_target_ips
-  description = "List of EFS mount target IPs (one per Availability Zone)"
-}
 
-output "efs_network_interface_ids" {
-  value       = module.efs.network_interface_ids
-  description = "List of mount target network interface IDs"
-}
+# output "efs_mount_target_ids" {
+#   value       = module.efs.mount_target_ids
+#   description = "List of EFS mount target IDs (one per Availability Zone)"
+# }
+
+# output "efs_mount_target_ips" {
+#   value       = module.efs.mount_target_ips
+#   description = "List of EFS mount target IPs (one per Availability Zone)"
+# }
+
+# output "efs_network_interface_ids" {
+#   value       = module.efs.network_interface_ids
+#   description = "List of mount target network interface IDs"
+# }
 
 
 
