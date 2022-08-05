dcc3a3c4b6c1493c715aef5d9c113933b336c133
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2022-08-05 16:06:35 -0700

    initial commit

--- main.tf
+++ main.tf
@@ -1,55 +1,40 @@
-
-resource "aws_efs_file_system" "default" {
-  #bridgecrew:skip=BC_AWS_GENERAL_48: BC complains about not having an AWS Backup plan. We ignore this because this can be done outside of this module.
-  # count                           = local.enabled ? 1 : 0
-  tags                            = var.tags
-  availability_zone_name          = var.availability_zone_name
+resource "aws_efs_file_system" "this" {
   encrypted                       = var.encrypted
   kms_key_id                      = var.kms_key_id
   performance_mode                = var.performance_mode
   provisioned_throughput_in_mibps = var.provisioned_throughput_in_mibps
   throughput_mode                 = var.throughput_mode
 
-  # dynamic "lifecycle_policy" {
-  #   for_each = var.transition_to_ia == "" ? [] : [1]
-  #   content {
-  #     transition_to_ia = var.transition_to_ia
-  #   }
-  # }
+  dynamic "lifecycle_policy" {
+    for_each = var.transition_to_ia == "" ? [] : [1]
+    content {
+      transition_to_ia = var.transition_to_ia
+    }
+  }
 
-  # # dynamic "lifecycle_policy" {
-  # #   for_each = var.transition_to_primary_storage_class == "" ? [] : [1]
-  # #   content {
-  # #     transition_to_primary_storage_class = var.transition_to_primary_storage_class
-  # #   }
-  # # }
+  tags = {
+    Name = var.file_system_name
+  }
 }
 
-resource "aws_efs_mount_target" "default" {
-  count          = length(var.subnets)
-  file_system_id = aws_efs_file_system.default.id
-  # ip_address     = var.mount_target_ip_address
+resource "aws_efs_mount_target" "this" {
+  count = length(var.subnets)
+
+  file_system_id = aws_efs_file_system.this.id
   subnet_id      = var.subnets[count.index]
+
   security_groups = var.security_groups
-  # security_groups = compact(
-  #   (concat(
-  #     [module.security_group.id],
-  #     var.associated_security_group_ids
-  #   ))
-  # )
 }
 
-resource "aws_efs_access_point" "default" {
-  for_each = var.access_points
-
-  file_system_id = aws_efs_file_system.default.id
+resource "aws_efs_access_point" "this" {
+  for_each       = var.access_points
+  file_system_id = aws_efs_file_system.this.id
 
   dynamic "posix_user" {
     for_each = null != lookup(each.value, "posix_user", lookup(var.access_points_defaults, "posix_user", null)) ? [1] : []
     content {
-      gid            = lookup(lookup(each.value, "posix_user", lookup(var.access_points_defaults, "posix_user", null)), "gid")
-      uid            = lookup(lookup(each.value, "posix_user", lookup(var.access_points_defaults, "posix_user", null)), "uid")
-      # secondary_gids = local.secondary_gids[each.key] != null ? split(",", local.secondary_gids[each.key]) : null
+      gid = lookup(lookup(each.value, "posix_user", lookup(var.access_points_defaults, "posix_user", null)), "gid")
+      uid = lookup(lookup(each.value, "posix_user", lookup(var.access_points_defaults, "posix_user", null)), "uid")
     }
   }
 
@@ -67,62 +52,23 @@ resource "aws_efs_access_point" "default" {
     }
   }
 
-  tags = var.tags
+  tags = {
+    Name = each.key
+  }
 }
 
-# module "security_group" {
-#   source  = "cloudposse/security-group/aws"
-#   version = "1.0.1"
+resource "aws_vpc_endpoint" "this" {
+  count = var.enable_vpc_endpoint ? 1 : 0
 
-#   enabled                       = local.security_group_enabled
-#   security_group_name           = var.security_group_name
-#   create_before_destroy         = var.security_group_create_before_destroy
-#   security_group_create_timeout = var.security_group_create_timeout
-#   security_group_delete_timeout = var.security_group_delete_timeout
+  service_name = "com.amazonaws.${var.aws_region}.elasticfilesystem"
+  vpc_id       = var.vpc_id
 
-#   security_group_description = var.security_group_description
-#   allow_all_egress           = true
-#   rules                      = var.additional_security_group_rules
-#   rule_matrix = [
-#     {
-#       source_security_group_ids = local.allowed_security_group_ids
-#       cidr_blocks               = var.allowed_cidr_blocks
-#       rules = [
-#         {
-#           key         = "in"
-#           type        = "ingress"
-#           from_port   = 2049
-#           to_port     = 2049
-#           protocol    = "tcp"
-#           description = "Allow ingress EFS traffic"
-#         }
-#       ]
-#     }
-#   ]
-#   vpc_id = var.vpc_id
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = var.security_groups
+  subnet_ids          = var.subnets
+  private_dns_enabled = true
 
-#   context = module.this.context
-# }
-
-# module "dns" {
-#   source  = "cloudposse/route53-cluster-hostname/aws"
-#   version = "0.12.2"
-
-#   enabled  = local.enabled && length(var.zone_id) > 0
-#   dns_name = var.dns_name == "" ? module.this.id : var.dns_name
-#   ttl      = 60
-#   zone_id  = try(var.zone_id[0], null)
-#   records  = [local.dns_name]
-
-#   context = module.this.context
-# }
-
-# resource "aws_efs_backup_policy" "policy" {
-#   count = module.this.enabled ? 1 : 0
-
-#   file_system_id = join("", aws_efs_file_system.default.*.id)
-
-#   backup_policy {
-#     status = var.efs_backup_policy_enabled ? "ENABLED" : "DISABLED"
-#   }
-# }
+  tags = {
+    Name = var.file_system_name
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,9 @@
+output "file_system_id" {
+  value       = aws_efs_file_system.this.id
+  description = "The file system ID"
+}
 
-
-# output "efs_arn" {
-#   value       = module.efs.arn
-#   description = "EFS ARN"
-# }
-
-# output "efs_id" {
-#   value       = module.efs.id
-#   description = "EFS ID"
-# }
-
-# output "efs_host" {
-#   value       = module.efs.host
-#   description = "Route53 DNS hostname for the EFS"
-# }
-
-
-
-
-# output "efs_mount_target_ids" {
-#   value       = module.efs.mount_target_ids
-#   description = "List of EFS mount target IDs (one per Availability Zone)"
-# }
-
-# output "efs_mount_target_ips" {
-#   value       = module.efs.mount_target_ips
-#   description = "List of EFS mount target IPs (one per Availability Zone)"
-# }
-
-# output "efs_network_interface_ids" {
-#   value       = module.efs.network_interface_ids
-#   description = "List of mount target network interface IDs"
-# }
-
-
-
+output "access_points" {
+  value       = aws_efs_access_point.this
+  description = "The access point list"
+}
